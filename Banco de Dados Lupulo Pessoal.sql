2e879bdd7955663f2052ef6798e44d6ff4a5ae11
abraaomarcos <abraao.m3@gmail.com>
2023-03-15 00:11:54 -0300

    alimentei a tabela cadastro 3 clientes

--- Banco de Dados Lupulo Pessoal.sql
+++ Banco de Dados Lupulo Pessoal.sql
@@ -38,3 +38,30 @@ INSERT INTO sensor VALUES
 (DEFAULT, 20231234, 'ativo');
 
 SELECT * FROM sensor;
+DROP TABLE cadastro;
+USE sprint1;
+DROP TABLE cadastro;
+CREATE TABLE cadastro(
+id INT PRIMARY KEY AUTO_INCREMENT,
+cnpj VARCHAR(20),
+cpf VARCHAR(11),
+nome VARCHAR(40) NOT NULL,
+email VARCHAR(40) NOT NULL,
+telefone VARCHAR(40) NOT NULL,
+cep VARCHAR(9),
+endereco VARCHAR(100),
+UF VARCHAR(9)
+);
+INSERT INTO cadastro VALUES
+(DEFAULT, '12345678000100', '12345678901',
+ 'Plantador', 'plantador@plantador.com',
+ '11912345678', '123456789', 'Rua dos plantadores, 21, Sao Gonçalo - Rio de Janeiro', 'RJ');
+ INSERT INTO cadastro VALUES
+ (DEFAULT, '12345678000101', '12345678902', 'Agricultor', 'agricultor@agricultura.com',
+ '11912345671', '123456781', 'Rua dos Agricultores, 20, Umarizal - Belém', 'PA');
+ INSERT INTO cadastro VALUES
+ (DEFAULT, '12345678000102', '12345678903', 'Colheitor', 'colheitor@colheita.com',
+ '11912345672','123456782', 'Rua dos colheitores, 44, Perpetuo Socorro - Macapá', 'AP');
+ SELECT * FROM cadastro;
+ SELECT * FROM sensor;
+ SELECT * FROM lupulo;
